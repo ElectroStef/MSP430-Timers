f62ce5196b10da73c548a17c1abb97454c9ca4ea
ElectroStef <31677212+ElectroStef@users.noreply.github.com>
2021-05-10 18:00:20 -0400

    changes

--- TimerMain.c
+++ TimerMain.c
@@ -1,4 +1,4 @@
-#include <msp430.h> 
+#include <msp430.h>
 
 void setUpTimerA0()
 {
@@ -21,35 +21,35 @@ void setUpTimerA2()
     TA2CTL = TASSEL__SMCLK |ID__4|MC__UP;
 }
 
+void setUpGPIOs()
+{
+  PJOUT &= ~0x01;
+  PJDIR |= 0x01;
+  PJOUT &= ~0x02;
+  PJDIR |= 0x02;
+  PJOUT &= ~0x04;
+  PJDIR |= 0x04;
+  PJOUT &= ~0x08;
+  PJDIR |= 0x08;
+  P1REN |= 0x01;
+  P1OUT &= ~0x01;
+  P1IE |= 0x01;
+  P1IES |= 0x01;
+  P1IFG &= ~0x01;
+}
+
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+  WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	PM5CTL0 &=  ~LOCKLPM5;
 
-    PJOUT &= ~0x01;
-    PJDIR |= 0x01;
+  setUpGPIOs();
+  setUpTimerA0();
+  setUpTimerA1();
+  setUpTimerA2();
 
-    PJOUT &= ~0x02;
-    PJDIR |= 0x02;
-
-    PJOUT &= ~0x04;
-    PJDIR |= 0x04;
-
-    PJOUT &= ~0x08;
-    PJDIR |= 0x08;
-
-    P1REN |= 0x01;
-    P1OUT &= ~0x01;
-    P1IE |= 0x01;
-    P1IES |= 0x01;
-    P1IFG &= ~0x01;
-
-    setUpTimerA0();
-    setUpTimerA1();
-    setUpTimerA2();
-
-    _bis_SR_register(LPM0_bits + GIE);
-    _no_operation();
+  _bis_SR_register(LPM0_bits + GIE);
+  _no_operation();
 
 }
 
@@ -77,5 +77,3 @@ __interrupt void pushButton()
     PJOUT ^= BIT3;
     P1IFG &= ~BIT0;
 }
-
-
