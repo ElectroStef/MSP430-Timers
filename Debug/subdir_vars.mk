04df466183dc0e071129d6c0fbb56d4eda16604d
ElectroStef <31677212+ElectroStef@users.noreply.github.com>
2021-05-10 16:08:55 -0400

    Initial commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5969.cmd 
+
+C_SRCS += \
+../TimerMain.c 
+
+C_DEPS += \
+./TimerMain.d 
+
+OBJS += \
+./TimerMain.obj 
+
+OBJS__QUOTED += \
+"TimerMain.obj" 
+
+C_DEPS__QUOTED += \
+"TimerMain.d" 
+
+C_SRCS__QUOTED += \
+"../TimerMain.c" 
+
+
---
+++ TimerMain.c
@@ -0,0 +1,82 @@
+#include <msp430.h> 
+
+void setUpTimerA0()
+{
+    TA0CCTL0 = CCIE;
+    TA0CCR0 = 51000;
+    TA0CTL = TASSEL__SMCLK |ID__4| MC__UP;
+}
+
+void setUpTimerA1()
+{
+    TA1CCTL0 = CCIE;
+    TA1CCR0 = 52000;
+    TA1CTL = TASSEL__SMCLK |ID__4 | MC__UP;
+}
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	PM5CTL0 &=  ~LOCKLPM5;
+
+    PJOUT &= ~0x01;
+    PJDIR |= 0x01;
+
+    PJOUT &= ~0x02;
+    PJDIR |= 0x02;
+
+    PJOUT &= ~0x04;
+    PJDIR |= 0x04;
+
+    PJOUT &= ~0x08;
+    PJDIR |= 0x08;
+
+    P1REN |= 0x01;
+    P1OUT &= ~0x01;
+    P1IE |= 0x01;
+    P1IES |= 0x01;
+    P1IFG &= ~0x01;
+
+    setUpTimerA0();
+
+
+
+    TA2CCTL0 = CCIE;
+    TA2CCR0 = 53000;
+    TA2CTL = TASSEL__SMCLK |ID__4|MC__UP;
+
+    _bis_SR_register(LPM0_bits + GIE);
+    _no_operation();
+
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void delay1 ()
+{
+  PJOUT ^= BIT0;
+}
+
+#pragma vector = TIMER2_A0_VECTOR
+__interrupt void delay2()
+{
+  PJOUT ^= BIT1;
+}
+
+#pragma vector = TIMER1_A0_VECTOR
+__interrupt void delay3()
+{
+  PJOUT ^= BIT2;
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void pushButton()
+{
+    PJOUT ^= BIT3;
+    P1IFG &= ~BIT0;
+}
+
+
