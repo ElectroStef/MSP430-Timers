dceda225580000b7283083c9a4da027bd732df5d
ElectroStef <31677212+ElectroStef@users.noreply.github.com>
2021-05-10 20:18:06 -0400

    clean up
    added header file

--- Release/TimerMain.d
+++ Release/TimerMain.d
@@ -6,6 +6,7 @@ TimerMain.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr5969.h
 TimerMain.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
 TimerMain.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics.h
 TimerMain.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics_legacy_undefs.h
+TimerMain.obj: C:/Projects/MSPS/Timers/TimerMain.h
 
 ../TimerMain.c: 
 C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h: 
@@ -13,3 +14,4 @@ C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr5969.h:
 C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics_legacy_undefs.h: 
+C:/Projects/MSPS/Timers/TimerMain.h: 
---
+++ TimerMain.h
@@ -0,0 +1,66 @@
+#ifndef TimerMain
+#define TimerMain
+
+void setUpTimerA0()
+{
+    TA0CCTL0 = CCIE;
+    TA0CCR0 = 51000;
+    TA0CTL = TASSEL__SMCLK |ID__4| MC__UP;
+}
+
+void setUpTimerA1()
+{
+    TA1CCTL0 = CCIE;
+    TA1CCR0 = 52000;
+    TA1CTL = TASSEL__SMCLK |ID__4 | MC__UP;
+}
+
+void setUpTimerA2()
+{
+    TA2CCTL0 = CCIE;
+    TA2CCR0 = 53000;
+    TA2CTL = TASSEL__SMCLK |ID__4|MC__UP;
+}
+
+void setUpGPIOs()
+{
+  PJOUT &= ~0x01;
+  PJDIR |= 0x01;
+  PJOUT &= ~0x02;
+  PJDIR |= 0x02;
+  PJOUT &= ~0x04;
+  PJDIR |= 0x04;
+  PJOUT &= ~0x08;
+  PJDIR |= 0x08;
+  P1REN |= 0x01;
+  P1OUT &= ~0x01;
+  P1IE |= 0x01;
+  P1IES |= 0x01;
+  P1IFG &= ~0x01;
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void delay1 ()
+{
+  PJOUT ^= BIT0;
+}
+
+#pragma vector = TIMER2_A0_VECTOR
+__interrupt void delay2()
+{
+  PJOUT ^= BIT1;
+}
+
+#pragma vector = TIMER1_A0_VECTOR
+__interrupt void delay3()
+{
+  PJOUT ^= BIT2;
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void pushButton()
+{
+    PJOUT ^= BIT3;
+    P1IFG &= ~BIT0;
+}
+#endif
